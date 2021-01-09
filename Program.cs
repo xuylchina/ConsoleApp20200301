37e793b277b0b32d7ac3f1e1b77a7cce195c5536
xuylchina <ntxuyalei@163.com>
2021-01-09 15:35:36 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp20200301
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("AMW");
+        }
+    }
+}
